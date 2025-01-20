9f5342e39cc585b2d7356c7569350d16fe8f6958
YJ_disco <jiaoyijia668@gmail.com>
2025-01-20 16:03:56 +0800

    index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,13 +1,13 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
-export const siteDescription = '记录回忆，知识和畅想的地方'
+export const siteName = 'YJ的知识库'
+export const siteShortName = 'YJ的知识库'
+export const siteDescription = '极致源于梦想，追求源于热爱'
 
 /** 文档所在目录 */
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/ferifering/nolebase'
 /** Discord */
 export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
